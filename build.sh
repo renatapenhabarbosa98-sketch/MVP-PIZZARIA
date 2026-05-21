5f67ba4cf02f77f94df903925961fbfd676b23fe
Renata <renatapenhabarbosa98@gmail.com>
2026-05-21 00:50:24 -0300

    mover migrate para startup, fix build

--- build.sh
+++ build.sh
@@ -4,14 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 cd backend
 python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-import os
-User = get_user_model()
-username = os.environ.get('ADMIN_USERNAME', 'admin')
-password = os.environ.get('ADMIN_PASSWORD', '')
-if password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, '', password)
-    print('Superuser criado')
-"
