c7e7604c49101ceb167fc64498b48e4dc2411129
Renata <renatapenhabarbosa98@gmail.com>
2026-05-21 00:23:35 -0300

    criar superuser admin no deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ pip install -r requirements.txt
 cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@pizzaria.com', 'pizzaria123')
+    print('Superuser criado')
+"
