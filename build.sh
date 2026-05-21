b95311e00bb6cdd46a743c800f032950d7da9109
Renata <renatapenhabarbosa98@gmail.com>
2026-05-21 00:27:22 -0300

    credenciais admin via variaveis de ambiente

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
+import os
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@pizzaria.com', 'pizzaria123')
+username = os.environ.get('ADMIN_USERNAME', 'admin')
+password = os.environ.get('ADMIN_PASSWORD', '')
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, '', password)
     print('Superuser criado')
 "
